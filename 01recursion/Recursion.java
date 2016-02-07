4e07afd0f779e148098666cbc708f704fd1cec41
Elina Hvirtsman <elina.hvirtsman@gmail.com>
2016-02-07 15:38:13 -0500

    Made Recursion class which implements hw01 interface

---
+++ 01recursion/Recursion.java
@@ -0,0 +1,22 @@
+public class Recursion implements hw01{
+
+    public String name(){
+	return "Hvirtsman,Elina";
+    }
+
+    public double sqrt(double n){
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	}
+	return sqrtHelper(n, n / 2);
+    }
+
+    public double sqrtHelper(double n, double guess){
+	if (guess * guess > n - .0001 && guess * guess < n + .0001){
+	    return guess;
+	} else {
+	    return sqrtHelper(n, (n / guess + guess) / 2);
+	}
+    }
+}
+	
