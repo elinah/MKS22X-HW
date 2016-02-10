ba2dff287fb67cfc53fa2dba0ad58ef3657232cb
Elina Hvirtsman <elina.hvirtsman@gmail.com>
2016-02-09 22:32:53 -0500

    Added add and remove methods to Board

--- 02NQueens/Board.java
+++ 02NQueens/Board.java
@@ -19,10 +19,47 @@ public class Board{
 	    }
 	    System.out.println(board[row][size-1]);
 	}
+	System.out.println();
+    }
+
+    public void addQueen(int row, int col){
+	if (board[row][col] == 0){
+	    board[row][col] = 1;
+	    for (int y=col+1,dev=1;y<size;y++,dev++){
+		board[row][y] -= 1;
+		if (row + dev < size){
+		    board[row+dev][y] -= 1;
+		}
+		if (row - dev > -1){
+		    board[row-dev][y] -= 1;
+		}
+	    }
+	}
+    }
+
+    public void removeQueen(int row, int col){
+	if (board[row][col] == 1){
+	    board[row][col] = 0;
+	    for (int y=col+1,dev=1;y<size;y++,dev++){
+		board[row][y] += 1;
+		if (row + dev < size){
+		    board[row+dev][y] += 1;
+		}
+		if (row - dev > -1){
+		    board[row-dev][y] += 1;
+		}
+	    }
+	}
     }
 
     public static void main(String[] args){
-	Board a = new Board(5);
+	Board a = new Board(4);
+	a.print();
+	a.addQueen(0,0);
+	a.print();
+	a.addQueen(2,1);
+	a.print();
+	a.removeQueen(2,1);
 	a.print();
     }
 }
