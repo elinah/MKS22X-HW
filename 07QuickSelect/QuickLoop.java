624d981980be20e08bae277fb24631e049327deb
Elina Hvirtsman <elina.hvirtsman@gmail.com>
2016-03-08 22:11:01 -0500

    Changed from looping to recursive function

--- 07QuickSelect/QuickLoop.java
+++ 07QuickSelect/QuickLoop.java
@@ -1,9 +1,10 @@
 import java.util.*;
 
-public class Quick{
+public class QuickLoop{
 
     private static int partition(int[] data, int left, int right){
 	int part = (int)(left + (Math.random() * (right-left+1)));
+	//System.out.println(data[part]);
 	int temp = data[right];
 	data[right] = data[part];
 	data[part] = temp;
@@ -26,34 +27,41 @@ public class Quick{
 	    temp = data[right+1];
 	    data[right+1] = data[end];
 	    data[end] = temp;
+	    //System.out.println(Arrays.toString(data));
 	    return right+1;
 	} else {
 	    temp = data[right];
 	    data[right] = data[end];
 	    data[end] = temp;
+	    //System.out.println(Arrays.toString(data));
 	    return right;
 	}
     }
 
     public static int quickselect(int[] data, int k){
-	return quickselect(data,k,0,data.length-1);
-    }
-    
-    public static int quickselect(int[] data, int k, int left, int right){
+	int left = 0;
+	int right = data.length-1; 
 	int part = partition(data,left,right);
-	if (k != part){
+	//System.out.println(part);
+	while (k != part){
 	    if (k < part){
-		return quickselect(data, k, left, part - 1);
+		right = part - 1;
+		//System.out.println("Hello " + left + ", " + right);
+		part = partition(data,left,right);
 	    } else {
-		return quickselect(data, k, part + 1, right);
+		left = part + 1;
+		//System.out.println("Hey " + left + ", " + right);
+		part = partition(data,left,right);
 	    }
+	    //System.out.println(part);
+	    //System.out.println(left + ", " + right);
 	}
 	return data[part];
     }
 
 		
     public static void main(String[] args){
-	Quick a = new Quick();
+	QuickLoop a = new QuickLoop();
 	int[] ary = {16,48,1,23,56,45};
 	System.out.println(a.quickselect(ary,0));
 	//System.out.println(a.partition(ary,0,13));
