9e916b60bd627308fe565cf47be5eb2ff7811e6d
Elina Hvirtsman <elina.hvirtsman@gmail.com>
2016-03-14 01:21:33 -0400

    Modified methods to account for duplicates

--- 07QuickSelect/Quick.java
+++ 07QuickSelect/Quick.java
@@ -6,7 +6,7 @@ public class Quick{
 	return "7,Hvirtsman,Elina";
     }
 
-    private static int partition(int[] data, int left, int right){
+    private static int partitionOld(int[] data, int left, int right){
 	int part = (int)(left + (Math.random() * (right-left+1)));
 	int temp = data[right];
 	data[right] = data[part];
@@ -39,20 +39,85 @@ public class Quick{
 	}
     }
 
-    public static int quickselect(int[] data, int k){
-	return quickselect(data,k,0,data.length-1);
+    public static int quickSelectOld(int[] data, int k){
+	return quickSelectOld(data,k,0,data.length-1);
     }
     
-    private static int quickselect(int[] data, int k, int left, int right){
-	int part = partition(data,left,right);
+    private static int quickSelectOld(int[] data, int k, int left, int right){
+	int part = partitionOld(data,left,right);
 	if (k != part){
 	    if (k < part){
-		return quickselect(data, k, left, part - 1);
+		return quickSelectOld(data, k, left, part - 1);
 	    } else {
-		return quickselect(data, k, part + 1, right);
+		return quickSelectOld(data, k, part + 1, right);
 	    }
 	}
 	return data[part];
+    }
+
+    public static void quickSortOld(int[] data){
+	quickSortOld(data,0,data.length-1);
+    }
+
+    private static void quickSortOld(int[] data, int left, int right){
+	if (right - left > 0){
+	    int part = partitionOld(data,left,right);
+	    quickSortOld(data,left,part - 1);
+	    quickSortOld(data,part + 1,right);
+	}
+    }
+
+    private static int[] partition(int[] data, int[] tempAry, int left, int right){
+	int[] retAry = new int[2];
+	if (left == right){
+	    retAry[0] = left;
+	    retAry[1] = right;
+	} else {
+	    int part = (int)(left + (Math.random() * (right-left+1)));
+	    //int[] tempAry = new int[data.length];
+	    int start = left;
+	    int end = right;
+	    for(int i = left;i < right+1;i++){
+		if (data[i] < data[part]){
+		    tempAry[start] = data[i];
+		    start++;
+		} else if (data[i] > data[part]){
+		    tempAry[end] = data[i];
+		    end--;
+		}
+	    }
+	    //System.out.println("tempAry: " + Arrays.toString(tempAry));
+	    int retLeft = start;
+	    while (start <= end){
+		tempAry[start] = data[part];
+		start++;
+	    }
+	    for(int i = left;i < right+1;i++){
+		data[i] = tempAry[i];
+	    }
+	    //System.out.println("data: " + Arrays.toString(data));
+	    retAry[0] = retLeft;
+	    retAry[1] = end;
+	}
+	//System.out.println("retAry: " + Arrays.toString(retAry));
+	return retAry;
+    }
+
+    public static int quickSelect(int[] data, int k){
+	return quickSelect(data,k,0,data.length-1);
+    }
+    
+    private static int quickSelect(int[] data, int k, int left, int right){
+	int[] tempAry = new int[data.length];
+	int[] part = partition(data,tempAry,left,right);
+	if (k != part[0]){
+	    if (k < part[0]){
+		return quickSelect(data, k, left, part[0] - 1);
+	    } else {
+		return quickSelect(data, k, part[1] + 1, right);
+	    }
+	}
+	return data[part[0]];
     }
 
     public static void quickSort(int[] data){
@@ -61,20 +126,31 @@ public class Quick{
 
     private static void quickSort(int[] data, int left, int right){
 	if (right - left > 0){
-	    int part = partition(data,left,right);
-	    quickSort(data,left,part - 1);
-	    quickSort(data,part + 1,right);
+	    int[] tempAry = new int[data.length];
+	    int[] part = partition(data,tempAry,left,right);
+	    quickSort(data,left,part[0] - 1);
+	    quickSort(data,part[1] + 1,right);
 	}
     }
 
 		
     public static void main(String[] args){
-	Quick a = new Quick();
-	int[] ary = {16,48,1,23,56,45};
-	a.quickSort(ary);
-	//System.out.println(a.quickselect(ary,0));
-	//System.out.println(a.partition(ary,0,13));
-	System.out.println(Arrays.toString(ary));
+	//int[] ary = {16,23,48,1,23,56,23,45,23,23,23,23,23,23,23,23};
+	//quickSort(ary);
+	//System.out.println(quickselect(ary,0));
+	//System.out.println(partition(ary,0,13));
+	//System.out.println(Arrays.toString(ary));
+        int[] d = new int [4000000];
+	int[] c = new int [d.length];
+
+	for(int i = 0; i < d.length; i++){
+	    d[i]= (int)(Math.random()*Integer.MAX_VALUE);
+	    c[i]= d[i];
+	}
+	quickSort(d); //or even your old quicksort!!!
+	Arrays.sort(c);
+	System.out.println("Done: Sorted="+Arrays.equals(d,c));
+	
     }
 }
 	
